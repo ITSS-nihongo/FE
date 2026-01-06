c9ca169ff262e51faf3149ae7764d61f26f31b1c
vnquang24 <vunhatquang2004@gmail.com>
2026-01-06 16:56:45 +0700

    fixx

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@hey-api/openapi-ts'
 
 export default defineConfig({
   client: 'fetch', // Use built-in fetch client
-  input: 'http://localhost:3000/doc', // Backend OpenAPI endpoint
+  input: process.env.NEXT_PUBLIC_API_URL + '/doc', // Backend OpenAPI endpoint
   output: {
     path: './lib/api/generated-openAPI',
     format: 'prettier',
